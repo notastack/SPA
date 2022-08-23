6c1a60133ed1832279f21e843bbd607c934d532f
notastack <103752556+notastack@users.noreply.github.com>
2022-08-23 21:34:41 +0200

    Create spalight.sh

---
+++ spalight.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+if [ -d .spa ]
+then
+	sleep 1
+else
+	mkdir .spa
+	sudo apt install apg > /dev/null ; sudo apt install openssl > /dev/null
+	alias spa='/home/workpad/spa/SPA/spa.sh'
+fi
+if [ -r ./.spa/$1.enc ];
+then
+	openssl rsautl -decrypt -in ./.spa/$1.enc -out ./.spa/$1 -inkey ./.spa/private-key.pem
+  cat ./.spa/$1
+	rm ./.spa/$1
+else
+	apg -M SNCL -m 10 -n 1 > ./.spa/$1
+  cat ./.spa/$1
+	openssl rsautl -encrypt -in ./.spa/$1 -out ./.spa/$1.enc -inkey ./.spa/public-key.pem -pubin || ( echo no key found, genrerating new ones. ;  openssl genrsa -out ./.spa/private-key.pem 2048 ; openssl rsa -pubout -in ./.spa/private-key.pem -out ./.spa/public-key.pem ; openssl rsautl -encrypt -in ./.spa/$1 -out ./.spa/$1.enc -inkey ./.spa/public-key.pem -pubin )
+	rm ./.spa/$1
+fi
